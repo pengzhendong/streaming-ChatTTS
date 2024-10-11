5706ddf9569aba8592e3771baf86393efc52a2c2
pengzhendong <275331498@qq.com>
2024-10-11 18:38:09 +0800

    refactor

--- main.py
+++ main.py
@@ -1,61 +1,46 @@
 import time
 
-import ChatTTS
 import torch
 import torchaudio
-
 from streaming_dvae import StreamingDVAE
+
+from gpt import GPT
 
 
 torch.manual_seed(6666)
-
-
-class RefineTextParams:
-    prompt = ""
-    top_P = 0.7
-    top_K = 20
-    temperature = 0.7
-    repetition_penalty = 1.0
-    max_new_token = 384
-    min_new_token = 0
-    show_tqdm = True
-    ensure_non_empty = True
-    manual_seed = None
-
-
-chat = ChatTTS.Chat()
-chat.load(compile=True)
-class InferCodeParams(RefineTextParams):
-    prompt: str = "[speed_5]"
-    spk_emb = chat.sample_random_speaker()
-    spk_smp = None
-    txt_smp = None
-    temperature = 0.1
-    repetition_penalty = 1.05
-    max_new_token = 2048
-    stream_batch = 4
-    stream_speed = 12000
-    pass_first_n_batches = 2
-
 sdvae = StreamingDVAE()
+gpt = GPT(
+    {
+        "hidden_size": 768,
+        "intermediate_size": 3072,
+        "num_attention_heads": 12,
+        "num_hidden_layers": 20,
+        "use_cache": False,
+        "max_position_embeddings": 4096,
+        "spk_emb_dim": 192,
+        "spk_KL": False,
+        "num_audio_tokens": 626,
+        "num_text_tokens": 21178,
+        "num_vq": 4,
+    },
+    "asset/Embed.safetensors",
+    "asset/gpt",
+    "asset/tokenizer",
+    device="cpu",
+)
 
 text = "This repo contains the code for annotating English Fisher Speech Part I and II Transcripts"
-begin = time.time()
-# text_tokens = chat._refine_text(text, "cpu", RefineTextParams()).ids
-# text_tokens = [i[i.less(chat.tokenizer.break_0_ids)] for i in text_tokens]
-# text = chat.tokenizer.decode(text_tokens)
-
 audios = []
 is_first = True
-num_frames = 0
-for result in chat._infer_code(text, True, "cpu", True, InferCodeParams()):
-    ids = result.ids[0].T[None, :, num_frames:]
-    num_frames += ids.shape[2]
-    for audio in sdvae.streaming_decode(ids):
+
+begin = time.time()
+for tokens in gpt.generate(text):
+    for audio in sdvae.streaming_decode(tokens):
         if is_first:
             is_first = False
-            print(f"{round(time.time() - begin, 3)}s, {num_frames}")
+            print(f"{round(time.time() - begin, 3)}s")
         audios.append(audio)
 audios.append(sdvae.decode_caches())
 audios = torch.cat(audios, dim=1)
+print(audios.shape)
 torchaudio.save("output.wav", audios.cpu(), 24000)
